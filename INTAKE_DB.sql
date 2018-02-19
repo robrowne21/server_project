670b0e35c332fa91c4fdb5777126c72aaafe497b
Robbie Browne <robrowne@tcd.ie>
2018-02-19 22:17:42 +0000

    remove duplicate files from root

--- INTAKE_DB.sql
+++
@@ -1,7 +0,0 @@
-create table INTAKE_DB (
-	id NUMBER(10) NOT NULL,
-	date_ DATE(255) NOT NULL,
-	meal_id NUMBER(10) NULL,
-	food_id NUMBER(10) NULL,
-	weight NUMBER(10) NULL,
-)
--- MEAL_DB.sql
+++
@@ -1,8 +0,0 @@
-create table MEAL_DB (
-	id NUMBER(10) NOT NULL,
-	meal_name VARCHAR(255) NOT NULL,
-	food_id_1 NUMBER(10) NOT NULL,
-	weight_1 NUMBER(10) NOT NULL,
-	food_id_2 NUMBER(10) NULL,
-	weight_2 NUMBER(10) NULL,
-)
